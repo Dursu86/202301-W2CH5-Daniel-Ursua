1e850145401edbdcb45fb9ddd991eddbee01c007
Daniel <dursuaf@gmail.com>
2023-01-29 20:03:04 +0100

    Add initial function

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ import { cellsGen } from './functions/board.js';
 import { cellsSplice } from './functions/board.js';
 import { frameCons } from './functions/board.js';
 
-import { checkAround } from './functions/position.js';
-import { isAlive } from './functions/position.js';
 import { newBoard } from './functions/position.js';
 import { initialChange } from './functions/initialChange.js';
 
 const liveAction = (num) => {
   const initialBoard = frameCons(cellsSplice(cellsGen(24), 6));
-  initialChange(num);
+  initialBoard();
+  const board = initialChange(initialBoard(), num);
+  const actualBoard = newBoard(board);
+  setInterval(newBoard(actualBoard), 200);
 };
+
+liveAction(24);
