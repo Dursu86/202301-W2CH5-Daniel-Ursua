d22feec517610f704fb51676eeeeefc9786a60df
Daniel <dursuaf@gmail.com>
2023-01-29 18:19:52 +0100

    Add position functions

--- functions/position.js
+++ functions/position.js
@@ -1,1 +1,41 @@
-export const checkStatus = (cell) => {};
+const checkAround = (board, index, element) => {
+  const around = [
+    board[index][element - 1],
+    board[index][element + 1],
+    board[index - 1][element - 1],
+    board[index - 1][element],
+    board[index - 1][element + 1],
+    board[index + 1][element - 1],
+    board[index + 1][element],
+    board[index + 1][element + 1],
+  ];
+  return around.filter((element) => element === 1).length;
+};
+
+const isAlive = (aliveCount, cell) => {
+  if (cell === 1 && (aliveCount === 2 || aliveCount === 3)) {
+    return 1;
+  }
+
+  if (cell === 0 && aliveCount === 3) {
+    return 1;
+  }
+
+  return 0;
+};
+
+const newBoard = (board) => {
+  const resultBoard = [];
+  for (let i = 1; i < board.length - 1; i++) {
+    const row = [];
+    for (let j = 1; j < board[i].length - 1; j++) {
+      const aliveCount = checkAround(board, i, j);
+      const change = isAlive(aliveCount, board[i][j]);
+      row.push(change);
+    }
+
+    resultBoard.push(row);
+  }
+
+  console.log(resultBoard);
+};
